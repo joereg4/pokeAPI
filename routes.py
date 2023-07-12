e08c7e099ab83325d59a4679e2221d071c457284
joereg4 <joereg4@protonmail.com>
2023-07-11 21:11:49 -0500

    Reduced pagination to previous and next

--- routes.py
+++ routes.py
@@ -9,21 +9,27 @@ pokemon_bp = Blueprint(
 @pokemon_bp.route('/')
 def index():
     pokemon_url = "https://pokeapi.co/api/v2/pokemon/?limit=20"
-    pagination_url = pokemon_url
-
-    pokemons, next_url = get_pokemon_data(pagination_url)
+    pokemons, next_url = get_pokemon_data(pokemon_url)
     return render_template('pokemon.html', pokemons=pokemons, next_url=next_url, prev_url=None)
 
 
 @pokemon_bp.route('/page')
 def page():
-    page_url = request.args.get('url')
-    pokemons, next_url = get_pokemon_data(page_url)
-    return render_template('pokemon.html', pokemons=pokemons, next_url=next_url, prev_url=page_url)
+    current_page_url = request.args.get('url')
+    pokemons, next_page_url = get_pokemon_data(current_page_url)
+    return render_template('pokemon.html', pokemons=pokemons, next_url=next_page_url, prev_url=current_page_url)
 
 
 @pokemon_bp.route('/next')
 def next_page():
-    next_url = request.args.get('url')
-    pokemons, next_url = get_pokemon_data(next_url)
-    return render_template('pokemon.html', pokemons=pokemons, next_url=next_url)
+    next_page_url = request.args.get('url')
+    pokemons, next_page_url_next = get_pokemon_data(next_page_url)
+    return render_template('pokemon.html', pokemons=pokemons, next_url=next_page_url_next, prev_url=next_page_url)
+
+
+@pokemon_bp.route('/prev')
+def prev_page():
+    prev_page_url = request.args.get('url')
+    pokemons, _ = get_pokemon_data(prev_page_url)
+    next_page_url = prev_page_url
+    return render_template('pokemon.html', pokemons=pokemons, next_url=next_page_url, prev_url=prev_page_url)
