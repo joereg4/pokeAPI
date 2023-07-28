745afa9b8d8bdcda585fa2f20877eb576cb6760a
joereg4 <joereg4@protonmail.com>
2023-07-27 20:20:34 -0500

    Adding test config to app.py

--- app.py
+++ app.py
@@ -9,8 +9,18 @@ logging.basicConfig(level=logging.INFO)
 utils.env.load_environment()
 
 
-def create_app():
+def create_app(test_config=None):
     app = Flask(__name__)
+
+    # If there's a test config, override the default configurations
+    if test_config:
+        app.config.update(test_config)
+    else:
+        # Use the default configurations
+        if utils.env.get_env_variable("FLASK_ENV") == "development":
+            app.config["DEBUG_PRINT_ROUTES"] = True
+        else:
+            app.config["DEBUG_PRINT_ROUTES"] = False
 
     with app.app_context():
         utils.get_db()
@@ -18,21 +28,20 @@ def create_app():
     app.teardown_appcontext(utils.close_db)
     app.register_blueprint(pokemon_bp)
 
-    # Set the configuration
-    if utils.env.get_env_variable("FLASK_ENV") == "development":
-        app.config["DEBUG_PRINT_ROUTES"] = True
+    if app.config["DEBUG_PRINT_ROUTES"]:
         for key, value in app.config.items():
             print(f"{key}: {value}")
             print("---- FLASK CONFIG END ----")
-    else:
-        app.config["DEBUG_PRINT_ROUTES"] = False
 
-    if app.config["DEBUG_PRINT_ROUTES"]:
         for rule in app.url_map.iter_rules():
             print(rule)
 
     return app
 
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run()
 
 if __name__ == '__main__':
     app = create_app()
