2ed3b0052fb1fead6c1c6e7a6f0d805f151ea6f2
joereg4 <joereg4@protonmail.com>
2025-07-27 12:43:56 -0500

    Also disable Pokemon card fetching on Pokemon detail pages

--- routes/pokemon.py
+++ routes/pokemon.py
@@ -440,7 +440,8 @@ def get_pokemon(id_or_name):
     )
 
     try:
-        cards = get_pokemon_cards(data["name"])
+        # cards = get_pokemon_cards(data["name"])
+        cards = []  # Temporarily disable Pokemon card fetching
     except Exception as e:
         # Log the exception and proceed with an empty list
         logging.debug(f"Error fetching cards for {data['name']}: {e}")
